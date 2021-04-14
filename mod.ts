0bd85c669686d194c4c90f15f99ac3b9ce777ec9
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-04-14 00:23:46 +0000

    Add dejavusans

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 /// <reference path="./deployctl.d.ts" />
 
-import { createCanvas } from "https://deno.land/x/canvas@v1.1.3/mod.ts";
+import Canvas, { EmulatedCanvas2D } from "https://deno.land/x/canvas@v1.1.3/mod.ts";
+import { DejaVueSans } from "./dejavusans.ts"
 // import { APIv2 } from "https://esm.sh/google-font-metadata@2.0.2";
 
 const APIv2 = {}
@@ -39,7 +40,7 @@ const fontSourceMap = new Map(fontSourceEntries);
 
 fontSourceMap.set(
   { family: "DejaVuSans", weight: "400", style: "normal", range: "latin" },
-  "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf",
+  DejaVueSans,
 );
 
 interface Params extends Font {
@@ -48,20 +49,30 @@ interface Params extends Font {
   color: string;
 }
 
-function measureTextWidth({ text, size, color, ...font }: Params): number {
-  const canvas = createCanvas(1, 1);
-  canvas.registerFont(fontSourceMap.get(font) || defaultFontSource, font);
+function registerFont(canvas: EmulatedCanvas2D, font: Font): void {
+  const source = fontSourceMap.get(font) || defaultFontSource;
+  if (source.match(new RegExp("https?://.*|data:font/ttf;base64,"))) {
+    fetch(source)
+      .then(response => response.blob())
+      .then(blob => blob.arrayBuffer())
+      .then(bytes => canvas.loadFont(bytes, font));
+  }
+}
+
+function measureTextWidth({ text, size, color: _, ...font }: Params): number {
+  const canvas = Canvas.MakeCanvas(1, 1);
+  registerFont(canvas, font);
   const context = canvas.getContext("2d");
   context.font = `"${font.weight}" "${font.style}"  "${size}" "${font.family}"`;
   return context.measureText(text).width;
 }
 
 function render({ text, size, color, ...font }: Params): Uint8Array {
-  const canvas = createCanvas(
+  const canvas = Canvas.MakeCanvas(
     measureTextWidth({ text, size, color, ...font }) + 20,
     parseInt(size) + 20,
   );
-  canvas.registerFont(fontSourceMap.get(font) || defaultFontSource, font);
+  registerFont(canvas, font);
   const context = canvas.getContext("2d");
   context.fillStyle = color;
   context.font =
