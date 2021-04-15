93207ade2182ea9de80f76c29040031f72545d7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-04-15 11:46:14 +0000

    Update for deno

--- poc.ts
+++ poc.ts
@@ -1,39 +1,39 @@
-const css = require('css')
-const traverse = require('traverse')
-const fetch = require('node-fetch')
-
-async function fetchStylesheet(family, weight) {
+import * as css from 'https://cdn.skypack.dev/css?dts';
+import { map, reduce } from 'https://cdn.skypack.dev/traverse?dts';
+import { encode } from 'https://deno.land/std/encoding/base64.ts'
+async function fetchStylesheet(family: string, weight: string) {
   const url = `https://fonts.googleapis.com/css2?family=${family}:wght@${weight}&display=swap`
   const response = await fetch(url)
   return await response.text()
 }
 
-async function fetchAllFonts(urls) {
+async function fetchAllFonts(urls: string[]) {
   const entriesPromised = urls.map(async (url) => {
     const response = await fetch(url)
-    const buffer = await response.buffer()
+    const buffer = await response.arrayBuffer()
+    const data = encode(buffer)
     const p = /(woff2|woff|ttf|otf)$/
     const m = p.exec(url)
-    const dataurl = `data:font/${m[0]};base64,${buffer.toString('base64')}`
+    const dataurl = `data:font/${m![0]};base64,${data}`
     return [url, dataurl]
   })
   return Object.fromEntries(await Promise.all(entriesPromised))
 }
 
-function extractUrls(tree) {
-  return traverse(tree).reduce(function(acc, x) {
+function extractUrls(tree: css.Stylesheet) {
+  return reduce(tree, function(acc, x) {
     const p = new RegExp("https://.*\\.(?:woff2|woff|ttf|otf)")
     const m = p.exec(x)
     return acc.concat(m ? m[0] : [])
   }, [])
 }
 
-async function generateStylesheet(family, weight) {
+async function generateStylesheet(family: string, weight: string) {
   const stylesheet = await fetchStylesheet(family, weight)
   const tree = css.parse(stylesheet)
   const urls = extractUrls(tree)
   const urlToDataurl = await fetchAllFonts(urls)
-  const newTree = traverse(tree).map(function(x) {
+  const newTree = map(tree, function(x) {
     const p = new RegExp("https://.*\\.(?:woff2|woff|ttf|otf)")
     const m = p.exec(x)
     if (m) {
