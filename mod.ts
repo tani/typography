1e429c0946ad3eb52bd209cbc6da7de1bdd9f0e7
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-04-15 15:27:11 +0000

    Remove js-traverse

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 /// <reference path="./deployctl.d.ts" />
 import * as csstree from "https://esm.sh/css-tree";
-import traverse from "https://cdn.skypack.dev/traverse?dts";
 import * as base64 from "https://deno.land/std/encoding/base64.ts";
 
 async function fetchStylesheetFromGoogleFonts(
@@ -13,27 +12,24 @@ async function fetchStylesheetFromGoogleFonts(
   return await response.text();
 }
 
-async function fetchAllRemoteFonts(
-  urls: string[],
-): Promise<Map<string, string>> {
-  const entriesPromised = urls.map(async (url): Promise<[string, string]> => {
-    const response = await fetch(url);
-    const buffer = await response.arrayBuffer();
-    const data = base64.encode(buffer);
-    const p = /(woff2|woff|ttf|otf)$/;
-    const m = p.exec(url);
-    const dataurl = `data:font/${m![0]};base64,${data}`;
-    return [url, dataurl];
-  });
-  return new Map(await Promise.all(entriesPromised));
+async function fetchRemoteFont(url: string): Promise<[string, string]> {
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+  const data = base64.encode(buffer);
+  const p = /(woff2|woff|ttf|otf)$/;
+  const m = p.exec(url);
+  return [url, `data:font/${m![0]};base64,${data}`];
 }
 
-function extractUrls(tree: csstree.CssNode) {
-  return traverse(tree).reduce(function (acc, x) {
-    const p = new RegExp("https://.*\\.(?:woff2|woff|ttf|otf)");
-    const m = p.exec(x);
-    return acc.concat(m ? m[0] : []);
-  }, []);
+async function fetchAllRemoteFonts(
+  tree: csstree.CssNode,
+): Promise<Map<string, string>> {
+  const entries: Promise<[string, string]>[] = [];
+  csstree.walk(tree, {
+    visit: "Url",
+    enter: (node) => entries.push(fetchRemoteFont(node.value.value)),
+  });
+  return new Map(await Promise.all(entries));
 }
 
 async function generateStylesheet(
@@ -42,16 +38,13 @@ async function generateStylesheet(
 ): Promise<string> {
   const stylesheet = await fetchStylesheetFromGoogleFonts(family, weight);
   const tree = csstree.parse(stylesheet);
-  const urls = extractUrls(tree);
-  const urlToDataurl = await fetchAllRemoteFonts(urls);
-  const newTree = traverse(tree).map(function (x) {
-    const p = new RegExp("https://.*\\.(?:woff2|woff|ttf|otf)");
-    const m = p.exec(x);
-    if (m) {
-      this.update(x.replace(m[0], urlToDataurl.get(m[0])));
-    }
+  const urlToDataurl = await fetchAllRemoteFonts(tree);
+  csstree.walk(tree, {
+    visit: "Url",
+    enter: (node) =>
+      node.value.value = urlToDataurl.get(node.value.value) || "",
   });
-  return csstree.generate(newTree);
+  return csstree.generate(tree);
 }
 
 interface Parameters {
@@ -64,7 +57,7 @@ interface Parameters {
 async function render(params: Parameters): Promise<string> {
   const stylesheet = await generateStylesheet(params.family, params.weight);
   const image = `<?xml version="1.0"?>
-<svg xmlns="http://www.w3.org/2000/svg" height='${parseInt(params.size)*1.3}'>
+<svg xmlns="http://www.w3.org/2000/svg" height='${parseInt(params.size) * 1.3}'>
 <style type="text/css"><![CDATA[${stylesheet}]]></style>
 <text x="10" y="${params.size}" font-size="${params.size}" font-family="${params.family}">${params.text}</text>
 </svg>`;
