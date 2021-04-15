c4b10e7ce007a15f12cc14f0b1b608e7bdb0f611
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-04-15 13:29:56 +0000

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,104 +1,92 @@
 /// <reference path="./deployctl.d.ts" />
+// @deno-types="https://esm.sh/@types/css/index.d.ts"
+import * as css from "https://esm.sh/css";
+import traverse from "https://cdn.skypack.dev/traverse?dts";
+import * as base64 from "https://deno.land/std/encoding/base64.ts";
 
-import Canvas, { EmulatedCanvas2D } from "https://deno.land/x/canvas@v1.1.3/mod.ts";
-import { DejaVueSans } from "./dejavusans.ts"
-// import { APIv2 } from "https://esm.sh/google-font-metadata@2.0.2";
-
-const APIv2 = {}
-
-interface Font {
-  family: string;
-  weight: string;
-  style: string;
-  range: string;
+async function fetchStylesheetFromGoogleFonts(
+  family: string,
+  weight: string,
+): Promise<string> {
+  const url =
+    `https://fonts.googleapis.com/css2?family=${family}:wght@${weight}&display=swap`;
+  const response = await fetch(url);
+  return await response.text();
 }
 
-const defaultFontSource = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf";
+async function fetchAllRemoteFonts(
+  urls: string[],
+): Promise<Map<string, string>> {
+  const entriesPromised = urls.map(async (url): Promise<[string, string]> => {
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
+    const data = base64.encode(buffer);
+    const p = /(woff2|woff|ttf|otf)$/;
+    const m = p.exec(url);
+    const dataurl = `data:font/${m![0]};base64,${data}`;
+    return [url, dataurl];
+  });
+  return new Map(await Promise.all(entriesPromised));
+}
 
-const listOfFontSourceEntries = Object.values(APIv2).map(
-  (metadata: any): [Font, string][] => {
-    const fontSourceEntries: [Font, string][] = [];
-    const family = metadata.family;
-    for (const weight of Object.keys(metadata.variants)) {
-      for (const style of Object.keys(metadata.variants[weight])) {
-        for (const range of Object.keys(metadata.variants[weight][style])) {
-          const source =
-            metadata.variants[weight][style][range].url["truetype"];
-          fontSourceEntries.push([{ family, weight, style, range }, source]);
-        }
-      }
+function extractUrls(tree: css.Stylesheet) {
+  return traverse(tree).reduce(function (acc, x) {
+    const p = new RegExp("https://.*\\.(?:woff2|woff|ttf|otf)");
+    const m = p.exec(x);
+    return acc.concat(m ? m[0] : []);
+  }, []);
+}
+
+async function generateStylesheet(
+  family: string,
+  weight: string,
+): Promise<string> {
+  const stylesheet = await fetchStylesheetFromGoogleFonts(family, weight);
+  const tree = css.parse(stylesheet);
+  const urls = extractUrls(tree);
+  const urlToDataurl = await fetchAllRemoteFonts(urls);
+  const newTree = traverse(tree).map(function (x) {
+    const p = new RegExp("https://.*\\.(?:woff2|woff|ttf|otf)");
+    const m = p.exec(x);
+    if (m) {
+      this.update(x.replace(m[0], urlToDataurl.get(m[0])));
     }
-    return fontSourceEntries;
-  },
-);
+  });
+  return css.stringify(newTree, { compress: true });
+}
 
-const fontSourceEntries = ([] as [Font, string][]).concat(
-  ...listOfFontSourceEntries,
-);
-
-const fontSourceMap = new Map(fontSourceEntries);
-
-fontSourceMap.set(
-  { family: "DejaVuSans", weight: "400", style: "normal", range: "latin" },
-  DejaVueSans,
-);
-
-interface Params extends Font {
+interface Parameters {
   text: string;
+  family: string;
   size: string;
-  color: string;
+  weight: string;
 }
 
-function registerFont(canvas: EmulatedCanvas2D, font: Font): void {
-  const source = fontSourceMap.get(font) || defaultFontSource;
-  if (source.match(new RegExp("https?://.*|data:font/ttf;base64,"))) {
-    fetch(source)
-      .then(response => response.blob())
-      .then(blob => blob.arrayBuffer())
-      .then(bytes => canvas.loadFont(bytes, font));
-  }
+async function render(params: Parameters): Promise<string> {
+  const stylesheet = await generateStylesheet(params.family, params.weight);
+  const image = `<?xml version="1.0"?>
+<svg xmlns="http://www.w3.org/2000/svg" height='${params.size}'>
+<style type="text/css"><![CDATA[${stylesheet}]]></style>
+<text x="10" y="${params.size}" font-size="${params.size}" font-family="${params.family}">${params.text}</text>
+</svg>`;
+  return image;
 }
 
-function measureTextWidth({ text, size, color: _, ...font }: Params): number {
-  const canvas = Canvas.MakeCanvas(1, 1);
-  registerFont(canvas, font);
-  const context = canvas.getContext("2d");
-  context.font = `"${font.weight}" "${font.style}"  "${size}" "${font.family}"`;
-  return context.measureText(text).width;
-}
-
-function render({ text, size, color, ...font }: Params): Uint8Array {
-  const canvas = Canvas.MakeCanvas(
-    measureTextWidth({ text, size, color, ...font }) + 20,
-    parseInt(size) + 20,
-  );
-  registerFont(canvas, font);
-  const context = canvas.getContext("2d");
-  context.fillStyle = color;
-  context.font =
-    `"${font.weight}" "${font.style}"  "${size}px" "${font.family}"`;
-  context.fillText(text, 10, parseInt(size) + 10);
-  return canvas.toBuffer();
-}
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", async (event) => {
   const url = new URL(event.request.url);
-  const params: Params = {
+  const params: Parameters = {
     text: "",
-    family: "DejaVuSans",
-    weight: "400",
-    style: "normal",
-    size: "10",
-    color: "black",
-    range: "latin",
+    family: "Antonio",
+    weight: "100",
+    size: "20",
     ...Object.fromEntries(url.searchParams),
   };
-  const body = render(params);
+  const body = await render(params);
   event.respondWith(
     new Response(body, {
       status: 200,
       headers: {
-        "content-type": "image/png",
+        "content-type": "image/svg+xml",
       },
     }),
   );
